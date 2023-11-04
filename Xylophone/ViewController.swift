3668258491922b8b76d75aad9bf6efbd7650f755
Fedor Igonin <murzik.212@yandex.ru>
2023-11-04 22:35:33 +0400

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            /*Снова сделаем непрозрачность отправителя полностью непрозрачной.
+             */
+            sender.alpha = 1.0
+            
+            
+            playSound(soundName: sender.currentTitle!)
+            
+        }
+        
+        
+        func playSound(soundName: String) {
+            let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        }
+    }
+    
 }
-
